cb958037b9defabd474e94b5469c7fbba11daa2c
HughOOZ <2560643316@qq.com>
2019-06-05 10:30:28 +0800

    380. Insert Delete GetRandom O(1)

---
+++ 380. Insert Delete GetRandom O(1)/solution.py
@@ -0,0 +1,33 @@
+'''We just keep track of the index of the added elements, so when we remove them, we copy the last one into it.
+
+From Python docs (https://wiki.python.org/moin/TimeComplexity) we know that list.append() takes O(1), both average and amortized. Dictionary get and set functions take O(1) average, so we are OK.'''
+
+import random
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.nums, self.pos = [], {}
+        
+    def insert(self, val):
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        return False
+        
+
+    def remove(self, val):
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.nums[idx], self.pos[last] = last, idx
+            self.nums.pop(); self.pos.pop(val, 0)
+            return True
+        return False
+            
+    def getRandom(self):
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+# 15 / 15 test cases passed.
+# Status: Accepted
+# Runtime: 144 ms
